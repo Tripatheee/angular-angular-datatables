df6749d642e4d68c995f67a0a10f59cddb85cffd
Tripatheee <atripathi1612@gmail.com>
2022-07-21 18:11:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-dgy3qc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-dgy3qc)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  constructor(private http: HttpClient) {}
+}
